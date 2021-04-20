24bc45def47d53c5404e832395ca5db3b58d7d87
XinDongBian <bxd_best@163.com>
2021-04-20 23:46:39 +0800

    归并排序的非递归，迭代实现

--- BinTree/SortTemplates.cpp
+++ BinTree/SortTemplates.cpp
@@ -3,7 +3,10 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
+#include <algorithm>
 
+#define _aaaaa
 
 int Paritition(int A[], int low, int high);//快排子函数
 int Paritition1(int A[], int low, int high);
@@ -174,7 +177,7 @@ void merge(std::vector<int>& nums, int start, int mid, int end)
 {
 	std::vector<int> leftArray(nums.begin() + start, nums.begin() + mid + 1);
 	std::vector<int> rightArray(nums.begin() + mid + 1, nums.begin() + end + 1);
-	//插入最大值为了比较
+	//插入最大值为了便于比较
 	leftArray.insert(leftArray.end(), std::numeric_limits<int>::max());
 	rightArray.insert(rightArray.end(), std::numeric_limits<int>::max());
 	int leftIndex = 0;
@@ -206,6 +209,52 @@ void mergeSort(std::vector<int>& nums,int start,int end )
 	}
 
 }
+//归并参考 https://blog.csdn.net/YF_Li123/article/details/75072991
+void mergeSort1(std::vector<int>& nums, int start, int end)
+{
+	if (start < end)
+	{
+		for (int step = 2; step / 2 < end; step *= 2)
+		{
+			for (int i = 0; i < end; i += step)
+			{
+
+			}
+		}
+	}
+}
+
+template<typename T> // 整數或浮點數皆可使用,若要使用物件(class)時必須設定"小於"(<)的運算子功能
+void merge_sort(T arr[], int len) {
+	T* a = arr;
+	T* b = new T[len];
+	for (int seg = 1; seg < len; seg += seg) {//seg为左区间或者右区间一组数据元素的个数；eg:seg = 1 代表一组数据左边是1个右边也是1个数据
+		for (int start = 0; start < len; start += seg + seg) {//以seg+seg为一组 能分的组数，对每组进行排序
+			int low = start;//起始下标
+			int mid = std::min(start + seg, len);//左区间的最大下标+1
+			int high = std::min(start + seg + seg, len);//右区间的最大下标+1
+			int k = low;
+			int start1 = low, end1 = mid;
+			int start2 = mid, end2 = high;
+			while (start1 < end1 && start2 < end2)
+				b[k++] = a[start1] < a[start2] ? a[start1++] : a[start2++];
+			while (start1 < end1)
+				b[k++] = a[start1++];
+			while (start2 < end2)
+				b[k++] = a[start2++];
+		}
+		T* temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a != arr) {
+		for (int i = 0; i < len; i++)
+			b[i] = a[i];
+		b = a;
+		std::cout << "test!" << std::endl;
+	}
+	delete[] b;
+}
 
 
 int main()
@@ -220,17 +269,10 @@ int main()
 	//print(v);
 	//QuickSort2(v, 0, 9);
 	//print(v);
-	mergeSort(v, 0, 9);
-	print(v);
+	/*mergeSort(v, 0, 9);
+	print(v);*/
+
+	merge_sort<int>(array, 10);
+	print(array,10);
+	//std::map<int ,int>
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
