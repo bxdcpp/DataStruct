8f8c9ba3558f6e6864888d698cdc9e32aba9566f
XinDongBian <bxd_best@163.com>
2021-06-01 00:38:20 +0800

    update graph

--- common/DataDefine.h
+++ common/DataDefine.h
@@ -12,6 +12,12 @@ Description:Provide  Data struct define
 #ifndef __DATADEFINE_H
 #define __DATADEFINE_H
 
+#define ERROR -1
+#define OK 0
+typedef  int status;
+
+
+
 //--------链表的顺序存储--------
 #define LIST_INIT_SIZE 100
 #define LISTINCREMENT 10
@@ -77,5 +83,29 @@ typedef struct BiTNode
 	TElemType data;
 	struct BiTNode* lchild, * rchild;
 }BiTNode,*BiTree;
+
+//-------图的数组(邻接矩阵)存储表示
+#define INFINITY INT_MAX   //最大值 
+#define MAX_VERTEX_NUM 20 //最大顶点
+
+typedef char InfoType;     //附加信息类型
+typedef int VRType;        //顶点关系类型
+typedef int VertexType;    //顶点数据类型
+
+typedef enum { DG, DN, UDG, UDN } GraphKind; //{有向图，有向网，无向图，无向网}
+
+typedef struct ArcCell {
+	VRType adj;     //顶点关系类型，对无权图用1或0表示是否相邻
+	//对带权图，则为权值类型
+	InfoType* info; //附加信息指针
+} ArcCell, AdjMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
+
+typedef struct {
+	VertexType vexs[MAX_VERTEX_NUM];//顶点向量
+	AdjMatrix arcs;     //邻接矩阵
+	int vexnum, arcnum;  //当前顶点数和弧数
+	GraphKind kind;     //图的种类
+} MGraph;
+
 
 #endif
